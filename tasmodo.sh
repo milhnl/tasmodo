b049d4aaf93b2da25ad4184a08fdb0560e9daa8c
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2021-10-14 09:30:53 +0200

    Init

---
+++ tasmodo.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+set -eu
+
+tasmota_mqtt_transport() {
+    mosquitto_pub -h "$MQTT_HOST" -t "cmnd/$1/Backlog" -m "$2"
+}
+
+tasmota_http_transport() {
+    curl "http://$1/cm?cmnd=$(echo "Backlog $2" | jq -sRr @uri)"
+}
+
+tasmodo() {
+    while getopts "h:t:c:" OPT "$@"; do
+        case "$OPT" in
+        h) MQTT_HOST="$OPTARG" ;;
+        t) TRANSPORT="$OPTARG" ;;
+        c) backlog="${backlog-}$(echo "$OPTARG" | sed 's/$/; /')" ;;
+        esac
+    done
+    shift "$(($OPTIND - 1))"
+    for host; do
+        case "${TRANSPORT-mqtt}" in
+        mqtt) tasmota_mqtt_transport "$host" "$backlog" ;;
+        http) tasmota_http_transport "$host" "$backlog" ;;
+        esac
+    done
+}
+
+tasmodo "$@"
